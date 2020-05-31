94cf44eaa060dafe535cfacebc8a406815a84416
Eddie Cunningham <edcunnin@umich.edu>
2020-05-31 09:21:03 -0400

    deleted old stuff

--- log_likelihood_results.py
+++
@@ -1,8 +0,0 @@
-import jax.numpy as jnp
-import pandas as pd
-
-# Compare log likelihoods on the validation set
-
-# Tune the value of s for each model on the test set to find the highest log likelihood
-
-def log_likelihood_comparison(key, data_loader, experiments, )
